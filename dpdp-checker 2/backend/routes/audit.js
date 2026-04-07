149ca853199d606483f1867938041d61136a71e9
Sumant Khalatkar <sumant@Sumants-MacBook-Air.local>
2026-04-07 11:21:27 +0530

    Refactor: Cleaned up repo structure and moved source to root

--- dpdp-checker 2/backend/routes/audit.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { runAudit } = require('../controllers/auditController');
-
-router.post('/', runAudit);
-
-module.exports = router;
